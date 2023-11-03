1e5204c75915f05e11e244b083085b792c3b936c
sarpsarpp <sarpguven@gmail.com>
2023-11-03 11:32:30 +0100

    test the change fields

--- src/main/java/com/example/demo/IQFeedService.java
+++ src/main/java/com/example/demo/IQFeedService.java
@@ -166,7 +166,7 @@ public class IQFeedService {
     }
 
     public void updateFields() throws IOException {
-        String command = "S,SELECT UPDATE FIELDS,Last,Change From Open,Percent Change,Change\r\n";
+        String command = "S,SELECT UPDATE FIELDS,Last,Percent Change\r\n";//percent change for @ES
         dataOut.println(command);
         dataOut.flush();  // Ensure the command is sent immediately
 
